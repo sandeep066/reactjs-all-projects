5f1f43f55591a78f1f578b3a7bfe48065cb0ef1c
sandeep066 <12527032+sandeep066@users.noreply.github.com>
2024-02-16 22:07:30 +0530

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,68 @@
+//App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+// import Services from "./Services";
+// import Contact from "./Contact";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+
+//Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
+//Home.js
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
+
+
+//About.js
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
+
+
